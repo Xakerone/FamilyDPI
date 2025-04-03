189d9e5942404cd3ad7f79fc03929c2476a730c4
_XakeR_ <114733445+Xakerone@users.noreply.github.com>
2025-04-03 17:11:40 +0300

    Add files via upload

--- UltimateFix.bat
+++ UltimateFix.bat
@@ -19,12 +19,11 @@ net session >nul 2>&1 || (
 
 :: Установка службы
 "%BIN%nssm.exe" install "FamilyDPI Service" "%BIN%winws.exe" ^
---wf-tcp=80,443 ^
---wf-udp=443,50000-65535 ^
+--wf-tcp=80,443 --wf-udp=443,50000-50100 ^
 --filter-udp=443 --hostlist="%LIST_PATH%" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
---filter-udp=50000-65535 --ipset="%DISCORD_IPSET_PATH%" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
+--filter-udp=50000-50100 --ipset="%DISCORD_IPSET_PATH%" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
 --filter-tcp=80 --hostlist="%LIST_PATH%" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
---filter-tcp=443 --hostlist="%LIST_PATH%" --dpi-desync=fake,split --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls="%BIN%tls_clienthello_www_google_com.bin"
+--filter-tcp=443 --hostlist="%LIST_PATH%" --dpi-desync=split2 --dpi-desync-split-seqovl=652 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern="%BIN%tls_clienthello_www_google_com.bin" --new ^
 
 ::schtasks /create /tn "Delete FamilyDPI Service" /sc ONSTART /ru SYSTEM /tr "\"D:\test\bin\nssm.exe\" remove \"FamilyDPI Service\" confirm" /f
 
